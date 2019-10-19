21d0466149170acacb8d74bb0620b6e011249c90
毫无波动 <896503709@qq.com>
2019-10-19 16:38:36 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
+import './assets/css/normalize.css'
+import './assets/css/border.css'
+
+
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
