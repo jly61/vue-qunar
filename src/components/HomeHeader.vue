e1beab60f9674fa37b4d05934de416535e011693
毫无波动 <896503709@qq.com>
2019-10-22 12:30:20 +0800

    add index-ajax

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -8,7 +8,7 @@
             输入城市/景点/游玩主题
         </div>
         <div class="header-right">
-            城市
+            {{city}}
             <span class="iconfont arrow-icon">&#xe64a;</span>
         </div>
     </div>
@@ -18,6 +18,11 @@
 <script>
     export default {
         name: "HomeHeader",
+        props: {
+            city: {
+                String
+            }
+        },
         data() {
             return {
                 height: 0
